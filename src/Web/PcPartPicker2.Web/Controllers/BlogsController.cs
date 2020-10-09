f78efae9934d031030c47f6980a6101f4eabf752
TodorNedkovski <tnedkovski@gmail.com>
2020-10-09 21:41:18 +0300

    Added Blog VIew

---
+++ src/Web/PcPartPicker2.Web/Controllers/BlogsController.cs
@@ -0,0 +1,28 @@
+﻿namespace PcPartPicker2.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using PcPartPicker2.Services.Data;
+    using PcPartPicker2.Web.ViewModels.Blogs;
+
+    public class BlogsController : Controller
+    {
+        private IBlogService blogService;
+
+        public BlogsController(IBlogService blogService)
+        {
+            this.blogService = blogService;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var blogs = await this.blogService.GetAllAsync<BlogViewModel>(3);
+
+            return this.View(blogs);
+        }
+    }
+}
